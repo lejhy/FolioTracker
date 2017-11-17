a3d16bf5c5d83af3a7993f125ce3e4fa86d60bf8
Heather McLean <szb15138@uni.strath.ac.uk>
2017-11-17 12:51:37 +0000

    created classes

---
+++ source/src/View.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class View extends IView {
+    JFrame frame;
+    JPanel panel1;
+    JButton refresh;
+    JButton createPortfolio;
+    JButton deletePortfolio;
+    JPanel portfolio;
+
+
+
+}
