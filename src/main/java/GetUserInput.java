6d4b7d269b77caf5c43b0a01f98cc2033cc8309d
Heather McLean <heather.mclean.2015@uni.strath.ac.uk>
2017-11-19 22:17:56 +0000

    Tried to implement user input for folio name

---
+++ src/main/java/GetUserInput.java
@@ -0,0 +1,49 @@
+package main.java;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GetUserInput extends JPanel {
+
+    private static JTextField nameField;
+
+    GetUserInput(){
+        JFrame frame = new JFrame("Test");
+
+        frame.setVisible(true);
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+
+        frame.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Enter name of Portfolio ");
+        label.setVisible(true);
+        frame.add(label);
+
+        nameField = new JTextField(15);
+        nameField.setVisible(true);
+        frame.add(nameField);
+
+        JButton btn = new JButton("Enter");
+        frame.add(btn);
+        btn.setVisible(true);
+        btn.addActionListener(new BtnListener());
+
+    }
+
+
+
+    private static class BtnListener implements ActionListener {
+
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+
+    }
+
+}
