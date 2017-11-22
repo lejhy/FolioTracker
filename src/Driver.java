30da5825e807dd95aedef5febf1c9b9f064ab334
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2017-11-22 17:42:06 +0000

    Controller removed

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,8 @@ public class Driver {
 
     public static void main(String[] args) {
         IMainFrame mainFrame = new MainFrame();
-        Controller control = new Controller(mainFrame);
 
+        mainFrame.addCreateFolioListener(new MainFrameButtonListener(MainFrameButtonListener.type.CREATE_FOLIO, mainFrame));
+        mainFrame.addOpenFolioListener(new MainFrameButtonListener(MainFrameButtonListener.type.OPEN_FOLIO, mainFrame));
     }
 }
