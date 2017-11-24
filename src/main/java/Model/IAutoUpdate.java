a535d7d018f82519b83e4566f47bfa0c7c6e142f
Aidan Bonnar CSM2015 <xpb14158@strath.ac.uk>
2017-11-24 23:57:47 +0000

    added method specs for IAutoUpdate, IFolio and IStock

--- src/main/java/Model/IAutoUpdate.java
+++ src/main/java/Model/IAutoUpdate.java
@@ -2,10 +2,23 @@ package Model;
 
 public interface IAutoUpdate {
 
+    /*
+    effects: if the auto-updater is already running return false
+             else start the auto-updater and return true
+     */
     boolean start();
 
+    /*
+    effects: if the auto-updater is not running return false
+             else stop the auto-updater and return true
+     */
     boolean stop();
 
+
+    /*
+    effects: if the auto-refresher is running return true
+             else return false
+     */
     boolean isRunning();
 
 }
